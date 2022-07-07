05b1669e1f23436266d3f7e05d641bc35e5f98a2
napiiey <napiiiey@gmail.com>
2022-07-07 23:06:28 +0900

    update

--- nwjs/src/js/database.js
+++ nwjs/src/js/database.js
@@ -1,2 +1,2 @@
-const database = [{number:22,date:"2022/7/7",modified:"",title:"静的サイトジェネレーターつきブログエディターを作ってみた1",tags:["javascript"],public:1}];
-    const allTags = ["javascript"];
+const database = [{number:22,date:"2022/7/7",modified:"",title:"静的サイトジェネレーターつきブログエディターを作ってみた1",tags:["javascript","Web"],public:1}];
+    const allTags = ["javascript","Web"];
